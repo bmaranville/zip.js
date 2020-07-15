ce5a7ecb690ae5db129c525359ebcc1ffacae0c1
Brian Ben Maranville <brian.maranville@nist.gov>
2020-07-15 11:45:17 -0400

    forgot the index file

---
+++ WebContent/index.js
@@ -0,0 +1,6 @@
+export {default as zip} from './zip.js';
+export {default as Inflater} from './inflate.js';
+export {default as Deflater} from './deflate.js';
+export {install as install_fs} from './zip-fs.js';
+export {install as install_ext} from './zip-ext.js';
+export {default as getMimeType} from './mime-types.js';
